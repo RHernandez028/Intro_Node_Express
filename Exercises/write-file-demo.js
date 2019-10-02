8fdac5c4cfcaee53cd03698fe67a2387e5d5998c
Gabriel <rhernadnez028@west-mec.org>
2019-10-01 18:27:34 -0700

    Started making server in step 10

---
+++ Exercises/write-file-demo.js
@@ -0,0 +1,14 @@
+
+var fs = require('fs');
+// var jsonString = '{ "name": "Gabriel"}';
+
+var jsonString = { 
+    name: 'Gabriel'
+};
+
+fs.writeFile('data2.json', JSON.stringify(jsonString),
+function(err) {
+    if(err){
+        console.log(err);
+    }
+});
