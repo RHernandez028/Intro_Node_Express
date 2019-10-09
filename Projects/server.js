bb8339a087eef814ac48a9e94ea5ae332310024c
Gabriel <rhernadnez028@west-mec.org>
2019-10-08 18:23:33 -0700

    made a server i think

--- Projects/server.js
+++ Projects/server.js
@@ -2,5 +2,6 @@
 var express = require('express');
 var app = express();
 var sever = app.listen(8080);
+//var fetch = require('node-fetch');
 app.use(express.static('website'));
-var require = require("request");
+
